574a71f14a26bbab8eeca8507a015d14204f4f1b
zcmerlin <merlin_chang@outlook.com>
2021-04-14 10:06:02 +0800

    dateview complete

--- src/components/InputDatePicker/DateView.js
+++ src/components/InputDatePicker/DateView.js
@@ -2,18 +2,37 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import ViewLayout from './ViewLayout'
 import DatePicker from './DatePicker'
+import HeaderTitle from './HeaderTitle'
 import { TertiaryButton, TertiaryIconButton } from '../Button'
 
+function modulo(m, n) {
+  return ((m % n) + n) % n;
+}
 function DateView(props) {
+  const { calendar, onSelectMonthYear } = props;
+  const { monthIndex, year } = calendar;
+
+  function incrementMonthIndex(increment) {
+    const incrementedMonthIndex = modulo(monthIndex + increment, 12);
+    const incrementedYear = year + Math.floor((monthIndex + increment) / 12);
+    onSelectMonthYear({
+      year: incrementedYear,
+      monthIndex: incrementedMonthIndex
+    });
+  }
+
+  const goToPreviousMonth = incrementMonthIndex.bind(null, -1);
+  const goToNextMonth = incrementMonthIndex.bind(null, 1);
+
   return (
     <ViewLayout
       header={{
-        leftElement: <TertiaryIconButton icon="arrowleft" />,
-        middleElement: <p>month year</p>,
-        rightElement: <TertiaryIconButton icon="arrowright" />
+        leftElement: <TertiaryIconButton icon="arrowleft" onClick={goToPreviousMonth} />,
+        middleElement: <HeaderTitle year={year} monthIndex={monthIndex} />,
+        rightElement: <TertiaryIconButton icon="arrowright" onClick={goToNextMonth} />
       }}
       bodyElement={
-        <DatePicker calendar={{year: 2021, monthIndex: 3}} selectedDate={new Date(2021, 3, 23)} />
+        <DatePicker calendar={calendar} selectedDate={new Date(2021, 3, 23)} />
       }
       footerElement={<TertiaryButton>Today</TertiaryButton>}
     />
@@ -21,7 +40,8 @@ function DateView(props) {
 }
 
 DateView.propTypes = {
-
+  calendar: DatePicker.propTypes.calendar,
+  onSelectMonthYear: PropTypes.func
 }
 
 export default DateView
--- src/components/InputDatePicker/InputDatePicker.js
+++ src/components/InputDatePicker/InputDatePicker.js
@@ -1,22 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import DateView from './DateView'
-import styled from 'styled-components'
-import { neutral, spacing } from '../../utils'
-
-const Picker = styled.div`
-  width: 29rem;
-  height: 35rem;
-  padding: ${spacing.padding.normal};
-  border: solid .1rem ${neutral[300]}
-`
+import Calendar from './Calendar'
 
 function InputDatePicker(props) {
-  return (
-    <Picker>
-      <DateView />
-    </Picker>
-  )
+  return <Calendar />
 }
 
 InputDatePicker.propTypes = {
